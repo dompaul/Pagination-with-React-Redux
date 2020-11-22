43011e3d500753f5daa5e777545458cc8c4d9557
Dominic Paul <dominic.paul@pulselive.com>
2020-11-22 12:12:21 +0000

    Remove unused imports

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,8 +1,3 @@
-import logo from '../assets/images/spacex-logo.png';
-import launchHomeImage from '../assets/images/launch-home.png';
-import launchHomeImageX2 from '../assets/images/launch-home@2x.png';
-import launchHomeImageX3 from '../assets/images/launch-home@3x.png';
-
 const CONSTANTS = {
     LABELS: {
         ERROR: 'Sorry, there has been an error trying to load the data.',
